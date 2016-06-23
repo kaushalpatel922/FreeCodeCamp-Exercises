b2c59003ba8402b7790a8bfcef22345ccd271689
Kaushal Patel <kaushalpatel922@gmail.com>
2016-06-22 20:09:06 -0400

    Using Bracket Notatino

---
+++ Basic Javascript/Bracket Notation to find nth character.js
@@ -0,0 +1,10 @@
+// Use Bracket Notation to Find the Last Character in a String.
+
+// Instructions
+// Use bracket notation to find the last character in the lastName variable.
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
