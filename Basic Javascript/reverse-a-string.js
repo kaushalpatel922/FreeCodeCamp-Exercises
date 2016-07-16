db1fd7457ebd57b1ed7f55d24b52b10f570fad82
Kaushal Patel <kaushalpatel922@gmail.com>
2016-07-16 15:29:05 -0400

    reversing a string

---
+++ Basic Javascript/reverse-a-string.js
@@ -0,0 +1,11 @@
+// Reverse the string.
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+reverseString("hello");
+// will result "olleh"
+
+reverseString("Howdy");
+// will result "ydwoH".
