090f84cbff1e071486ec64c94c21f664df2cfeb6
Kaushal Patel <kaushalpatel922@gmail.com>
2016-07-18 14:39:36 -0400

    remove value from an initial array

---
+++ Basic Javascript/seek-and-destroy.js
@@ -0,0 +1,14 @@
+// You will be provided with an initial array (the first argument in the destroyer function), followed by one or more arguments. Remove all elements from the initial array that are of the same value as these arguments.
+
+function destroyer(arr) {
+
+  var args = Array.prototype.slice.call(arguments);
+  args.splice(0,1);
+
+  return arr.filter(function (value) {
+    return args.indexOf(value) === -1;
+  });
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+// should result [1,1]
