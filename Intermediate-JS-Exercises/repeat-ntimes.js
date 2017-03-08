8bc7188a8e5c4514491c6b937ce6ce4b2c840bca
Kaushal Patel <kaushalpatel922@gmail.com>
2017-03-08 15:28:53 -0500

    repeat function n times

---
+++ Intermediate-JS-Exercises/repeat-ntimes.js
@@ -0,0 +1,13 @@
+//Create a function that logs a strings on how many times an integer has been entered.
+
+function repeat(times) {
+  var string = '';
+
+  for (var i = 0; i < times; i++){
+    string += this;
+  }
+  return string;
+}
+
+console.log('HelloWorld'.repeat(2));
+//should result HelloWorldHelloWorld.
